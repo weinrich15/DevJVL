bac707f5c3790facd3d4cdaabb5cedbdfa4d5e1d
Andrew Mark Feferman <57198537+mfeferman-cx@users.noreply.github.com>
2021-07-16 01:38:07 -0500

    Update EmailCheck.java

--- src/main/java/org/cysecurity/cspf/jvl/controller/EmailCheck.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/EmailCheck.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  * Feferman - added to trip full scan
+ * Feferman #2
  */
 
 package org.cysecurity.cspf.jvl.controller;
